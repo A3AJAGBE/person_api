afd546fd32da3995d48eb57a2e209418ba076e4c
Aminat Ajagbe <ajagbeaminat95@gmail.com>
2023-09-11 15:33:03 +0100

    Add edit_person and delete_person db function

--- crud.py
+++ crud.py
@@ -21,3 +21,18 @@ def create_person(db: Session, person: schemas.PersonBase):
     db.commit()
     db.refresh(new_person)
     return new_person
+
+
+def edit_person(db: Session, user_id: int, edit_person: schemas.PersonBase):
+    db_person = db.query(models.Person).filter(
+        models.Person.user_id == user_id).first()
+    db_person.name = edit_person.name
+    db.commit()
+    db.refresh(db_person)
+    return db_person
+
+
+def delete_person(db: Session, user_id: int):
+    db.query(models.Person).filter(models.Person.user_id == user_id).delete()
+    db.commit()
+    return f"Person with id number: {user_id} was deleted."
