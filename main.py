6ad39d919654709d746327222aa2dda7401f0316
Aminat Ajagbe <ajagbeaminat95@gmail.com>
2023-09-11 09:52:00 +0100

    Add Person basemodel and add_person function

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
+from pydantic import BaseModel
 
 app = FastAPI()
+
+persons = []
+
+class Person(BaseModel):
+    id: int
+    name: str
 
 
 @app.get("/")
 def index():
-    return {"Hello and Welcome to task two"}
+    return persons
+
+
+@app.post("/api")
+async def add_person(new_person: Person, res: Response):
+    person = new_person.model_dump()
+    persons.append(person)
+    res.status_code = 201
+    return "Person added successfully"
