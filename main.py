c493ce84d2d90e30440b335fb4b68720384d52ae
Aminat Ajagbe <ajagbeaminat95@gmail.com>
2023-09-11 12:16:21 +0100

    Refactor commit

--- main.py
+++ main.py
@@ -3,10 +3,22 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-persons = []
+persons = [
+    {
+        "name": "tola",
+        "user_id": 1
+    },
+    {
+        "name": "ngozi",
+        "user_id": 2
+    },
+    {
+        "name": "simi",
+        "user_id": 3
+    }
+]
 
 class Person(BaseModel):
-    id: int
     name: str
 
 
@@ -18,11 +30,19 @@ async def index():
 @app.post("/api")
 async def add_person(new_person: Person, res: Response):
     person = new_person.model_dump()
+    person["user_id"] = len(persons) + 1
     persons.append(person)
     res.status_code = 201
     return "Person added successfully"
 
 
-@app.get("/api/{id}")
-async def get_person(id: int, res: Response):
-    return persons[id-1]
+@app.get("/api/{user_id}")
+async def get_person(user_id: int, res: Response):
+    for person in persons:
+        if person["user_id"] == user_id:
+            res.status_code = 200
+            return person
+        else:
+            res.status_code = 404
+            return "Person not found"
+            
