46c4aa8e0b41bcdaa2db8c188f61edbfaeed32c6
Aminat Ajagbe <ajagbeaminat95@gmail.com>
2023-09-12 13:16:39 +0100

    Add update_person tests

--- test_sql.py
+++ test_sql.py
@@ -1,4 +1,3 @@
-import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -41,6 +40,10 @@ def test_create_person():
     assert "user_id" in data
     user_id = data["user_id"]
     assert data["user_id"] is not None
+    assert res.json() == {
+        "name": "mark essien",
+        "user_id": 1
+    }
 
 def test_create_person_invalid():
     res = client.post("/api", json={"name": 8})
@@ -50,13 +53,18 @@ def test_create_person_invalid():
 def test_create_person_empty():
     res = client.post("/api", json={"name": ""})
     assert res.status_code == 400
+    assert res.json() == {
+        "detail": "Name can't be empty"
+    }
     
 
 def test_create_person_exist():
     res = client.post("/api", json={"name": "mark essien"})
     assert res.status_code == 400
+    assert res.json() == {
+        "detail": "Name exist already"
+    }
     
-
 def test_index():
     res = client.get("/")
     assert res.status_code == 200
@@ -103,3 +111,47 @@ def test_read_person_by_name_not_found():
 def test_read_person_by_name_invalid():
     res = client.get(f"/api/name=jane%20doe")
     assert res.status_code == 422
+
+
+def test_update_person():
+    create_person = client.post("/api", json={"name": "doyin lawal"})
+    data = create_person.json()
+    assert create_person.status_code == 200
+    user_id = data["user_id"]
+    assert create_person.json() == {
+        "name": "doyin lawal",
+        "user_id": 2
+    }
+    
+    res = client.put(f"/api/{user_id}", json={"name": "bisi adekunmi"})
+    update_data = res.json()
+    assert res.status_code == 200
+    assert res.json() == {
+        "name": "bisi adekunmi",
+        "user_id": 2
+    }
+
+
+def test_update_person_empty():
+    res = client.put("/api/1", json={"name": ""})
+    assert res.status_code == 400
+    assert res.json() == {
+        "detail": "Name can't be empty"
+    }
+    
+
+def test_update_person_not_found():
+    res = client.put(f"/api/20", json={"name": "toni pain"})
+    assert res.status_code == 404
+    assert res.json() == {
+        "detail": "Person not found"
+    }
+
+
+def test_update_person_exist():
+    res = client.put("/api/1", json={"name": "mark essien"})
+    assert res.status_code == 400
+    assert res.json() == {
+        "detail": "Name exist already"
+    }
+
