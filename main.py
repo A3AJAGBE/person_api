d69ab8d43fb4e62a40696e2633492875d5058f65
Aminat Ajagbe <ajagbeaminat95@gmail.com>
2023-09-13 22:43:49 +0100

    Update update_person function

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import re
+PATTERN = re.compile("^([a-z]{2,}\s[a-z]{1,}'?-?[a-z]{2,}\s?([a-z]{1,})?)")
+
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
-import crud, models, schemas
+import crud
+import models
+import schemas
 from database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
@@ -17,19 +22,25 @@ def get_db():
     finally:
         db.close()
 
+PATTERN_ERROR_MSG = "Characters must include lowercase characters and space. Apostrophe and hyphen allowed"
+NOT_FOUND_ERROR_MEG = "Person not found"
+ALREADY_EXIST_MSG = "Name exist already"
+
 
 @app.post("/api", response_model=schemas.Person)
 def create_person(person: schemas.PersonBase, db: Session = Depends(get_db)):
-    if person.name == "":
-        raise HTTPException(status_code=400, detail="Name can't be empty")
     
-    new_person = crud.get_person_by_name(db, name=person.name)
-    
-    if new_person:
-        raise HTTPException(status_code=400, detail="Name exist already")
-    
-    return crud.create_person(db=db, person=person)
+    if not PATTERN.match(person.name):
+        raise HTTPException(
+            status_code=400, detail=PATTERN_ERROR_MSG)
 
+    new_person = crud.get_person_by_name(db, name=person.name)
+
+    if new_person:
+        raise HTTPException(status_code=400, detail=ALREADY_EXIST_MSG)
+
+    return crud.create_person(db=db, person=person)
+    
 
 @app.get("/", response_model=list[schemas.Person])
 def index(db: Session = Depends(get_db)):
@@ -40,40 +51,53 @@ def index(db: Session = Depends(get_db)):
 @app.get("/api/{user_id}", response_model=schemas.Person)
 def read_person(user_id: int, db: Session = Depends(get_db)):
     db_person = crud.get_person(db, user_id=user_id)
+    
     if db_person is None:
-        raise HTTPException(status_code=404, detail="Person not found")
+        raise HTTPException(status_code=404, detail=NOT_FOUND_ERROR_MEG)
+    
     return db_person
 
 
 @app.get("/api", response_model=schemas.Person)
 def read_person_by_name(name: str, db: Session = Depends(get_db)):
     db_person = crud.get_person_by_name(db, name=name)
+    
     if db_person is None:
-        raise HTTPException(status_code=404, detail="Person not found")
+        raise HTTPException(status_code=404, detail=NOT_FOUND_ERROR_MEG)
+    
     return db_person
 
 
 @app.put("/api/{user_id}", response_model=schemas.Person)
 def update_person(user_id: int, edit_person: schemas.PersonBase, db: Session = Depends(get_db)):
-    if edit_person.name == "":
-        raise HTTPException(status_code=400, detail="Name can't be empty")
-    
+    if not PATTERN.match(edit_person.name):
+        raise HTTPException(
+            status_code=400, detail=PATTERN_ERROR_MSG)
+
     db_person = crud.get_person(db, user_id=user_id)
     
     if db_person is None:
-        raise HTTPException(status_code=404, detail="Person not found")
+        raise HTTPException(status_code=404, detail=NOT_FOUND_ERROR_MEG)
     
     if db_person.name == edit_person.name:
-        raise HTTPException(status_code=400, detail="Name exist already")
+        raise HTTPException(
+            status_code=400, detail="No change in the name")
     
+    get_edit_name = crud.get_person_by_name(db, name=edit_person.name)
+        
+    if get_edit_name:
+        raise HTTPException(status_code=400, detail=ALREADY_EXIST_MSG)
+
     return crud.edit_person(db=db, user_id=user_id, edit_person=edit_person)
 
 
 @app.delete("/api/{user_id}")
 def delete_person(user_id: int, db: Session = Depends(get_db)):
     db_person = crud.get_person(db, user_id=user_id)
-    
+
     if db_person is None:
-        raise HTTPException(status_code=404, detail=f"Person with id number: {user_id}, not found.")
+        
+        raise HTTPException(
+            status_code=404, detail=f"Person with id number: {user_id}, not found.")
+        
     return crud.delete_person(db, user_id=user_id)
-    
